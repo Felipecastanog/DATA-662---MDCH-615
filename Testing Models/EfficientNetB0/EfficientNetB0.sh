0d9f45605e3499dcf803e04aa53883672d86bedf
gken11 <gracekennedy11@gmail.com>
2025-04-20 13:47:28 -0600

    Add files via upload

---
+++ Testing Models/EfficientNetB0/EfficientNetB0.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=1
+#SBATCH --partition=gpu
+#SBATCH --time=8:00:00
+#SBATCH --mem=32GB
+#SBATCH --job-name=EfficientNetB0
+#SBATCH --mail-type=END
+#SBATCH --mail-user=grace.kennedy1@ucalgary.ca
+#SBATCH --output=slurmoutput_%j.out
+
+echo starting slurm
+
+date
+id
+
+echo start initialization
+conda activate DATA622
+echo finished initializing
+
+# Execute python code
+python EfficientNetB0.py
+
+echo ending slurm
+date
